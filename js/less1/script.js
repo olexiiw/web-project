85126b8e92ed9d10450eea57301820bbc6ef8262
olexiiw <olexiisteamcity@gmail.com>
2026-05-02 12:47:18 +0300

    lesson1 JS

---
+++ js/less1/script.js
@@ -0,0 +1,46 @@
+console.log("Привіт. Це мій перший скрипт.")
+
+let age = 16;
+console.log("Мій вік:", age);
+age = 17;
+console.log("Тепер мій вік:", age);
+
+const birthYear = 2009;
+console.log("Рік народження:", birthYear);
+
+const userName = "Олександр"
+console.log("Ім'я", userName);
+console.log(typeof userName);
+
+const score = 10;
+const pi = 3.14;
+console.log(typeof score);
+console.log(typeof pi);
+
+const isStudent = true;
+console.log("ви студент?", isStudent);
+console.log(typeof isStudent);
+
+let userCity;
+console.log(typeof userCity);
+// console.log(userCity);
+
+let userCar = null;
+console.log(userCar);
+
+const apples = 5;
+const pears = 10;
+let totalFruits = apples + pears;
+console.log("Всього фруктів:", totalFruits);
+
+const firstName = "Тарас";
+const lastName = "Шевченко";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+const result = "text" + 123;
+console.log(result);
+
+const number_1 = 5;
+const number_2 = 2;
+console.log(number_1 + number_2);
